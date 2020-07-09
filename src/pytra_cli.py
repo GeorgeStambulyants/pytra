5e993d62e76a64b5816b8e300d0fc3bf353db2ac
George Stambulyants <george.stamb16@gmail.com>
2020-07-09 18:46:34 +0300

    Add basic functionality

--- src/pytra_cli.py
+++ src/pytra_cli.py
@@ -1,11 +1,23 @@
 import click
+from googletrans import Translator
+
+
+translator = Translator()
 
 
 @click.command()
 @click.option('-f', '--translate_from', default='auto')
 @click.option('-t', '--translate_to', default='en')
-def pytra(translate_from, translate_to):
-    click.echo(f'Translate from: {translate_from}, Translate to: {translate_to}')
+@click.argument('origin_text')
+def pytra(translate_from, translate_to, origin_text):
+    translated_obj = translator.translate(
+        text=origin_text,
+        origin=translate_from,
+        dest=translate_to,
+    )
+    translated_text = translated_obj.text
+
+    click.echo(translated_text)
 
 
 if __name__ == '__main__':
