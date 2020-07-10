6baef8f4b445973ba618923c16edc11afac74210
George Stambulyants <george.stamb16@gmail.com>
2020-07-10 19:55:32 +0300

    Add help messages

--- src/pytra_cli.py
+++ src/pytra_cli.py
@@ -1,9 +1,16 @@
 import click
-from googletrans import LANGUAGES
+from googletrans import LANGCODES, LANGUAGES
 
 from utils.translator import (
     get_translated_text,
     get_detected_language_obj
+)
+from utils.constants import (
+    TRANSLATE_FROM_HELP,
+    TRANSLATE_TO_HELP,
+    SHOW_LANGUAGE_CODES_HELP,
+    TRANSLATE_HELP,
+    DETECT_HELP
 )
 
 
@@ -12,9 +19,9 @@ def pytra():
     pass
 
 
-@pytra.command()
-@click.option('-f', '--translate_from', default='auto')
-@click.option('-t', '--translate_to', default='en')
+@pytra.command(help=TRANSLATE_HELP)
+@click.option('-f', '--translate_from', default='auto', help=TRANSLATE_FROM_HELP)
+@click.option('-t', '--translate_to', default='en', help=TRANSLATE_TO_HELP)
 @click.argument('origin_text')
 def translate(translate_from, translate_to, origin_text):
     try:
@@ -28,7 +35,7 @@ def translate(translate_from, translate_to, origin_text):
         click.echo(f'Got an exception: {e}')
 
 
-@pytra.command()
+@pytra.command(help=DETECT_HELP)
 @click.argument('sentence')
 def detect(sentence):
     detected_language_obj = get_detected_language_obj(sentence)
@@ -38,6 +45,19 @@ def detect(sentence):
         f'"{LANGUAGES[detected_language].title()}" was detected with '
         f'{detected_language_confidence} confidence'
     )
+
+
+@pytra.command(help=SHOW_LANGUAGE_CODES_HELP)
+@click.argument('language', required=False)
+def show_language_codes(language):
+    if language:
+        language = language.strip().lower()
+        try:
+            click.echo(f'{language} -- "{LANGCODES[language]}"')
+        except KeyError:
+            click.echo(f'Incorrect language')
+    else:
+        click.echo('All of them')
 
 
 if __name__ == '__main__':
---
+++ src/utils/constants.py
@@ -0,0 +1,11 @@
+#
+# Help messages for options
+#
+TRANSLATE_FROM_HELP = 'Language code to translate from. Default: "auto".'
+TRANSLATE_TO_HELP = 'Language code to translate to. Default: "en".'
+
+# Help messages for commands
+SHOW_LANGUAGE_CODES_HELP = 'Show language codes. If language argument is provided ' \
+                     'show only for provided language.'
+TRANSLATE_HELP = 'Translate stuff'
+DETECT_HELP = 'Detect languages'
