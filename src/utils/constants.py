7ab3e775424216e9418360bf7921b10451ea136b
George Stambulyants <george.stamb16@gmail.com>
2020-07-11 18:00:05 +0300

    Switch to triple-quoted strings and add additional help message

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,10 +6,19 @@ OUTPUT_FILE_NAME_HELP = 'Specify output file name'
 TRANSLATED_FILE_LANGUAGE_HELP = 'Language code to translate to. Default: "en".'
 
 # Help messages for commands
-SHOW_LANGUAGE_CODES_HELP = 'Show language codes. If language argument is provided ' \
-                     'show only for provided language.'
 TRANSLATE_HELP = 'Translate stuff'
 DETECT_HELP = 'Detect languages'
-TRANSLATE_FILE_COMMAND_HELP = 'Text file to translate. ' \
-                              'By default to "en" from "auto". ' \
-                              'Use -t option to specify language of the output file'
+SHOW_LANGUAGE_CODES_HELP = '''
+                              Show language codes. 
+                              If language argument is provided
+                              show only for provided language.
+                           '''
+TRANSLATE_FILE_COMMAND_HELP = '''
+                                 Text file to translate.
+                                 By default to "en" from "auto".
+                                 Use -t option to specify language 
+                                 of the output file.
+                                 By default pytra looks for the file
+                                 ii current directory. But it is possible
+                                 to specify relative or absolute path too.
+                              '''
