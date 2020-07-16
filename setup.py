06596ac90a3ab626f1798776e7aaffc8185078d1
George Stambulyants <george.stamb16@gmail.com>
2020-07-16 20:07:13 +0300

    Initial setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,46 @@
-from setuptools import setup
+import pathlib
+import io
+import os
+from setuptools import setup, find_packages
 
+
+
+HERE = pathlib.PATH(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+with io.open(os.path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [
+    req.strip() for req in all_reqs
+    if not req.startswith('#')
+    and not req.startswith('-')
+]
 
 setup(
-    name='pytra_cli',
+    name='pytra',
     version='0.1',
-    py_modules=['pytra_cli'],
+    description='Simple command-line tool for translating',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/GeorgeStambulyants/pytra',
+    author='GeorgeStambulyants',
+    author_email='george.stamb16@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packeges=find_packages(),
+    include_package_data=True,
     install_requires=[
         'click',
         'googletrans',
     ],
+    python_requires='>=3.7',
     entry_points='''
         [console_scripts]
-        pytra_cli=pytra_cli:pytra
+        pytra=src.pytra:pytra
     '''
 )
